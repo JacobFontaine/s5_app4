3f5df5a318945821ded4c2dc9b5587dd4afb0a2c
JacobFontaine <jacob.fontaine@gmail.com>
2018-02-26 14:40:35 -0500

    nom qui ont de l'allure

--- test.m
+++ test.m
@@ -1,10 +1,6 @@
 N = 16;
-current_min_rms = 1000;
-min_rms_M = 0;
-
-open_matrix = [3.27; 3.24; 3.10; 2.83; 2.52; 2.13; 1.76; 1.41; 1.08; 0.804; 0.561; 0.424; 0.301; 0.222; 0.165; 0.124]';
-coeff_matrix = [0 0.4 0.8 1.2 1.6 2 2.4 2.8 3.2 3.6 4.0 4.4 4.8 5.2 5.6 6.0]';
-
+tension_matrix = [3.27; 3.24; 3.10; 2.83; 2.52; 2.13; 1.76; 1.41; 1.08; 0.804; 0.561; 0.424; 0.301; 0.222; 0.165; 0.124]';
+distance_matrix = [0 0.4 0.8 1.2 1.6 2 2.4 2.8 3.2 3.6 4.0 4.4 4.8 5.2 5.6 6.0]';
 for M = 6
     P = zeros(N, M);
    
@@ -13,12 +9,12 @@ for M = 6
             if colonne == 1
                 P(rangee, colonne) = 1;
             else
-                P(rangee, colonne) = open_matrix(rangee).^(colonne-1);
+                P(rangee, colonne) = tension_matrix(rangee).^(colonne-1);
             end
         end
     end
     
-    A = (inv(P'*P)*P')*coeff_matrix;
+    A = (inv(P'*P)*P')*distance_matrix;
     result = 0;
     total = 0;
     
@@ -26,10 +22,10 @@ for M = 6
         result = 0;
         % TROUVER LE POLYNOME + REMPLACEMENT DE VALEUR
         for exp = 1:M
-            result = result + A(exp)*open_matrix(index).^(exp-1);
+            result = result + A(exp)*tension_matrix(index).^(exp-1);
         end
        
-        total = total + (result - coeff_matrix(index))^2;
+        total = total + (result - distance_matrix(index))^2;
     end
   
     % On sait qu'on prend M=6, donc je sauvegarde les valeurs
@@ -37,19 +33,19 @@ for M = 6
         used_A = A;
     end
     
-    % REPRESENTATION GRAPHIQUE
     poly = 0;
-    syms opening
+    syms x
     % POLYNOME POUR M=X
     for exp = 1:M
-        poly = poly + A(exp)*opening.^(exp-1);
+        poly = poly + A(exp)*x.^(exp-1);
     end
     
 end
 
 disp(used_A);
-opening = 0:0.1:3.3;
-plot(open_matrix,coeff_matrix,'o',opening,subs(poly,opening));
+% REPRESENTATION GRAPHIQUE
+x = 0:0.1:3.3;
+plot(tension_matrix,distance_matrix,'o',x,subs(poly,x));
 title('Données mesurées superposées de la fonction polynomiale calculée')
 ylabel('Variation(cm) de la hauteur du floteur par rapport à lhorizontale')
 xlabel('Tension(V)')
