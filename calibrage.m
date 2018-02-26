1ec35fac310a114e0c20b6fe953f7c6436b7a747
JacobFontaine <jacob.fontaine@gmail.com>
2018-02-26 15:40:29 -0500

    beau graphique pis unuseful comments

--- calibrage.m
+++ calibrage.m
@@ -1,22 +1,23 @@
 close all
 clear all
-
+%On veut toute les valeurs possible sur 16bits
 range = 0:(2^16 - 1);
 range = range.* (3.3 / 65535);
 poly = [-0.2566 2.2669 -7.5798 11.9577 -10.0720 6.9938];
 distance = polyval(poly, range);
-plot(d);
-
+figure(1)
+plot(range,distance);
+title('d en fonction de NV')
+ylabel('Niveau liquide(cm)')
+xlabel('Tension(V)')
+%On calcule chaque marche sur toute les valeurs
 for i = 1:65535;
-    step_size(i) = d(i) - d(i + 1);
+    step_size(i) = distance(i) - distance(i + 1);
 end
 
-lel = range(2:end);
-plot(lel, step_size);
-% syms x
-% poli = 6.9938 - 10.0720*x + 11.9577*x.^2 - 7.5798*x.^3 + 2.2669*x.^4 - 0.2566*x.^5;
-% 
-% x = range;
-% 
-% plot(x, subs(poli, x), 'o');
-
+x = range(2:end);
+figure(2)
+plot(x, step_size);
+title('Hauteur des marches en fonction de NV')
+ylabel('Hauteur(cm) des marches')
+xlabel('Tension(V)')
