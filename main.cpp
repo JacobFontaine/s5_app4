7aa48ba2a9e6be8ab48f451e316d73cbb77c9a61
JacobFontaine <jacob.fontaine@gmail.com>
2018-02-26 14:21:36 -0500

    16bits

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@ AnalogIn an1(p20);
 int main() {
      
   while (1){
-		float value = an1.read()*3.3;
-		float poly = 54 - (6.9938 +  -10.0720*value + 11.9577*powf(value, 2) +  -7.5798*powf(value, 3)
-								+ 2.2669*powf(value, 4) + -0.2566*powf(value, 5));
-		printf("Value: %f\r\n",value);
+		uint16_t value = an1.read_u16();
+		float decimalValue = 0.00005035477 * value ;
+		float poly = 54 - (6.9938 +  -10.0720*decimalValue + 11.9577*powf(decimalValue, 2) +  -7.5798*powf(decimalValue, 3)
+								+ 2.2669*powf(decimalValue, 4) + -0.2566*powf(decimalValue, 5));
+		printf("Value: %f\r\n",decimalValue);
 		printf("Poly: %f\r\n\n",poly);
 		wait(0.2);
 	}
